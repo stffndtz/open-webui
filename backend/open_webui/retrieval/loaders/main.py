f1ce65af2030f2f99b1b57a72163abb780912c55
Steffen Dietz <hello@stffndtz.de>
2025-08-20 16:27:43 +0200

    try another mode?

--- backend/open_webui/retrieval/loaders/main.py
+++ backend/open_webui/retrieval/loaders/main.py
@@ -346,7 +346,7 @@ class Loader:
                 api_key=self.kwargs.get("DOCUMENT_INTELLIGENCE_KEY"),
                 api_model='prebuilt-read',
                 api_version='2024-11-30',
-                mode='page' # we do this so the loader doesn't try to get the text from the lines instead of the content
+                mode='single' # we do this so the loader doesn't try to get the text from the lines instead of the content
                 # TODO: add support for multi-page documents
             )
         elif (
