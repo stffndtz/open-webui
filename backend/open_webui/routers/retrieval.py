91a64116efd3858a9375e0cffa64a5cbfe706e08
Steffen Dietz <hello@stffndtz.de>
2025-08-21 14:35:19 +0200

    add instead of bulk

--- backend/open_webui/routers/retrieval.py
+++ backend/open_webui/routers/retrieval.py
@@ -1153,6 +1153,10 @@ def save_docs_to_vector_db(
         f"save_docs_to_vector_db: document {_get_docs_info(docs)} {collection_name}"
     )
 
+    log.info(
+        f"save_docs_to_vector_db: vector_db {VECTOR_DB_CLIENT}"
+    )
+
     # Check if entries with the same hash (metadata.hash) already exist
     if metadata and "hash" in metadata:
         result = VECTOR_DB_CLIENT.query(
