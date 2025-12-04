95af159e0f12ede5281f237d463b3eeb12b38a7e
Steffen Dietz <hello@stffndtz.de>
2025-12-04 19:15:45 +0100

    missed env var

--- backend/open_webui/routers/retrieval.py
+++ backend/open_webui/routers/retrieval.py
@@ -92,6 +92,7 @@ from open_webui.utils.auth import get_admin_user, get_verified_user
 from open_webui.config import (
     ENV,
     RAG_EMBEDDING_MODEL_AUTO_UPDATE,
+    RAG_RERANKING_MODEL_AUTO_UPDATE,
     RAG_EMBEDDING_MODEL_TRUST_REMOTE_CODE,
     UPLOAD_DIR,
     DEFAULT_LOCALE,
