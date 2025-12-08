ee37c6e5d40b2e8bf26a5f1c68302b75961361bf
Steffen Dietz <hello@stffndtz.de>
2025-12-08 16:23:37 +0100

    updates rag model injetc

--- backend/open_webui/routers/retrieval.py
+++ backend/open_webui/routers/retrieval.py
@@ -1028,7 +1028,7 @@ async def update_rag_config(
                     reranking_model=request.app.state.config.RAG_OPENAI_RERANKER_MODEL,
                     external_reranker_url=request.app.state.config.RAG_EXTERNAL_RERANKER_URL,
                     external_reranker_api_key=request.app.state.config.RAG_EXTERNAL_RERANKER_API_KEY,
-                    auto_update=request.app.state.config.RAG_RERANKING_MODEL_AUTO_UPDATE,
+                    auto_update=RAG_RERANKING_MODEL_AUTO_UPDATE,
                     openai_reranker_api_key=request.app.state.config.RAG_OPENAI_RERANKER_API_KEY,
                     openai_reranker_url=request.app.state.config.RAG_OPENAI_RERANKER_URL,
                     openai_reranker_model=request.app.state.config.RAG_OPENAI_RERANKER_MODEL
